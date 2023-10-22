6326b81b07392340fdf90075d6922f4040b6f5ea
Crabsir <irbatir@gmail.com>
2023-10-22 20:11:19 +0200

    Updated Spy logic

--- mail/Spy.java
+++ mail/Spy.java
@@ -14,14 +14,14 @@ public class Spy implements MailService {
 
     @Override
     public Sendable processMail(Sendable mail) {
-        if (mail instanceof MailMessage &&
-            (mail.getFrom().equals(AUSTIN_POWERS) || mail.getTo().equals(AUSTIN_POWERS))
-        ) {
-            logger.log(Level.WARNING, "Detected target mail correspondence: from {0} to {1} \"{2}\"",
-                new Object[] {mail.getFrom(), mail.getTo(), ((MailMessage) mail).getMessage()});
-        } else {
-            logger.log(Level.INFO, "Usual correspondence: from {0} to {1}",
-                new Object[] {mail.getFrom(), mail.getTo()});
+        if (mail instanceof MailMessage) {
+            if (mail.getFrom().equals(AUSTIN_POWERS) || mail.getTo().equals(AUSTIN_POWERS)) {
+                logger.log(Level.WARNING, "Detected target mail correspondence: from {0} to {1} \"{2}\"",
+                        new Object[]{mail.getFrom(), mail.getTo(), ((MailMessage) mail).getMessage()});
+            } else {
+                logger.log(Level.INFO, "Usual correspondence: from {0} to {1}",
+                        new Object[]{mail.getFrom(), mail.getTo()});
+            }
         }
         return mail;
     }

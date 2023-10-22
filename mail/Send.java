94a5e71deb8c1e08b7724d417c59c934133c477f
Crabsir <irbatir@gmail.com>
2023-10-22 16:51:50 +0200

    Added Send

---
+++ mail/Send.java
@@ -0,0 +1,13 @@
+package mail;
+
+import java.util.logging.*;
+
+public class Send {
+
+    public static final String AUSTIN_POWERS = "Austin Powers";
+    public static final String WEAPONS = "weapons";
+    public static final String BANNED_SUBSTANCE = "banned substance";
+
+    public static void main(String[] args) {}
+
+}

b2fc25320c0c150f249fdcf740bc99fe5cdcd653
Crabsir <irbatir@gmail.com>
2023-10-22 16:58:59 +0200

    Added IllegalPackageException

---
+++ mail/IllegalPackageException.java
@@ -0,0 +1,13 @@
+package mail;
+
+public class IllegalPackageException extends RuntimeException {
+
+    public IllegalPackageException(String message) {
+        super(message);
+
+    }
+
+    public IllegalPackageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

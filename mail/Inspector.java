7ae1616ff777593e90e94ddcfcf6e69d9afc8517
Crabsir <irbatir@gmail.com>
2023-10-22 19:48:57 +0200

    Updated Inspector

--- mail/Inspector.java
+++ mail/Inspector.java
@@ -1,4 +1,21 @@
 package mail;
 
-public class Inspector {
+public class Inspector implements MailService {
+
+    private static final String WEAPONS = "weapons";
+    private static final String BANNED_SUBSTANCE = "banned substance";
+
+    public Inspector() {}
+    @Override
+    public Sendable processMail(Sendable mail) {
+        if (mail instanceof MailPackage) {
+            String c = ((MailPackage) mail).getContent().getContent();
+            if (c.contains(WEAPONS) || c.contains(BANNED_SUBSTANCE)) {
+                throw new IllegalPackageException("Illegal content: " + c);
+            } else if (c.contains("stones")) {
+                throw new StolenPackageException("Stolen content: " + c);
+            }
+        }
+        return mail;
+    }
 }

c561ac0958e795ebc628e699213f21c9aa9f1cda
Crabsir <irbatir@gmail.com>
2023-10-22 17:40:27 +0200

    Updated UntrustworthyMailWorker

--- mail/UntrustworthyMailWorker.java
+++ mail/UntrustworthyMailWorker.java
@@ -1,4 +1,24 @@
 package mail;
 
-public class UntrustworthyMailWorker {
+public class UntrustworthyMailWorker implements MailService {
+
+    private final MailService[] chain;
+    private final RealMailService rms;
+
+    public UntrustworthyMailWorker(MailService[] chain) {
+        this.chain = chain;
+        this.rms = new RealMailService();
+    }
+
+    public RealMailService getRealMailService() {
+        return rms;
+    }
+
+    @Override
+    public Sendable processMail(Sendable mail) {
+        for (MailService service: chain) {
+            mail = service.processMail(mail);
+        }
+        return getRealMailService().processMail(mail);
+    }
 }

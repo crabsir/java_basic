6ddc3f0d1fc6e31de7fac8dcdc9910d770298d11
Crabsir <irbatir@gmail.com>
2023-10-22 19:35:28 +0200

    Updated Thief

--- mail/Thief.java
+++ mail/Thief.java
@@ -1,4 +1,26 @@
 package mail;
 
-public class Thief {
+public class Thief implements MailService {
+
+    private final int minValue;
+
+    private int stolenValue;
+
+    public Thief(int minValue) {
+        this.minValue = minValue;
+        this.stolenValue = 0;
+    }
+
+    public int getStolenValue() {
+        return stolenValue;
+    }
+    @Override
+    public Sendable processMail(Sendable mail) {
+        if (mail instanceof MailPackage && ((MailPackage) mail).getContent().getPrice() >= minValue) {
+            stolenValue += ((MailPackage) mail).getContent().getPrice();
+            return new MailPackage(mail.getFrom(), mail.getTo(),
+                new Package("stones instead of " + ((MailPackage) mail).getContent().getContent(), 0));
+        }
+        return mail;
+    }
 }

3f8477f3d001bb3803bb8a2dff54d6469b2b93de
Crabsir <irbatir@gmail.com>
2023-10-24 02:46:55 +0200

    Updated Animal

--- animal/Animal.java
+++ animal/Animal.java
@@ -1,6 +1,6 @@
 package animal;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.Objects;
 
 public class Animal implements Serializable {
@@ -17,4 +17,17 @@ public class Animal implements Serializable {
         }
         return false;
     }
+
+    public static Animal[] deserializeAnimalArray(byte[] data) {
+        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) {
+            Animal[] a = new Animal[ois.readInt()];
+            for (int i = 0; i < a.length; i++) {
+                a[i] = (Animal) ois.readObject();
+            }
+            return a;
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
 }

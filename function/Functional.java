02135f57aa9efdce15b5cead4f1cb6749e116644
Crabsir <irbatir@gmail.com>
2023-10-24 23:08:34 +0200

    Added Functional

---
+++ function/Functional.java
@@ -0,0 +1,16 @@
+package function;
+
+import java.util.function.Predicate;
+import java.util.function.Function;
+
+public class Functional {
+
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+
+        return t -> condition.test(t) ? ifTrue.apply(t) : ifFalse.apply(t);
+
+    }
+}

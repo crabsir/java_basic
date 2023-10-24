fc4e373ddfbb0040bd915d758c20d5c7b77b114b
Crabsir <irbatir@gmail.com>
2023-10-25 00:09:19 +0200

    Updated with ternaryOperator usage

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,15 @@
+import java.util.Objects;
+import function.Functional;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		String f = "default";
+		String s = Functional.ternaryOperator(Objects::isNull, x -> f, Object::toString).apply(null);
+
+		System.out.println(s);
 	}
+
 
 }

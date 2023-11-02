f1a3a1acf518cafe95dd5369422783aac99b6463
Crabsir <irbatir@gmail.com>
2023-11-02 20:49:59 +0100

    Added MailMessage

---
+++ message/MailMessage.java
@@ -0,0 +1,9 @@
+package message;
+
+public class MailMessage extends Sendable<String> {
+
+    public MailMessage(String from, String to, String content) {
+        super(from, to, content);
+    }
+
+}
